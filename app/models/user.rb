2079a45b1b4da3f20d7220b2e15b795896e7951b
iogr <antgorbunov@gmail.com>
2021-01-12 17:57:22 +0300

    name recover if not valid on edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 35}
 
   before_validation :set_name, on: :create
+  after_validation :sanitized_username, on: :update
 
   private
 
   def set_name
     self.name = "Твой юзер №#{rand(10000)}" if self.name.blank?
+  end
+
+  def sanitized_username
+    self.name = User.find(self.id).name if !valid?(:name)
   end
 end

dad18601db177c6fa85346c5f0fca20b690c4d08
iogr <antgorbunov@gmail.com>
2021-01-15 01:42:12 +0300

    minus spare

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 35}
 
   before_validation :set_name, on: :create
-  after_validation :sanitized_username, on: :update
 
   def valid_name
     # pry.binding
     # "123"
     name
-  end
-
-  def sanitized_username
-    self.name = User.find(self.id).name if !valid?(:name)
   end
 end

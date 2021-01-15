d499f95f5382de3eac3e9af5e0c61c0e2ef16f00
iogr <antgorbunov@gmail.com>
2021-01-15 03:34:08 +0300

    added set_default_name method in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,24 @@
 class User < ApplicationRecord
+  MAXIMUM_NAME_LENGTH = 35
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :events
 
-  validates :name, presence: true, length: {maximum: 35}
+  validates :name, presence: true, length: {maximum: MAXIMUM_NAME_LENGTH}
 
   before_validation :set_name, on: :create
 
-  def valid_name
-    # pry.binding
-    # "123"
-    name
+  after_validation :set_default_name
+
+  def set_default_name
+    self.name = self.email[/.+(?=@.+)/] if self.name.length > MAXIMUM_NAME_LENGTH
+  end
+
+  private
+
+  def set_name
+    self.name = self.email[/.+(?=@.+)/] if self.name.blank?
   end
 end

17a60ea3a70cb68620c33bd0d41ebd8875e3a5ff
iogr <antgorbunov@gmail.com>
2021-01-28 22:13:57 +0300

    removed spare code in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,5 @@ class User < ApplicationRecord
 
   has_many :events
 
-  validates :name, presence: true, length: {maximum: MAXIMUM_NAME_LENGTH}
-
-  before_validation :set_name, on: :create
-
-  after_validation :set_default_name, on: :update
-
-  def set_default_name
-    self.name = self.email[/.+(?=@.+)/] if self.name.length > MAXIMUM_NAME_LENGTH
-  end
-
-  private
-
-  def set_name
-    self.name = self.email[/.+(?=@.+)/] if self.name.blank?
-  end
+  validates :name, presence: true, length: { maximum: MAXIMUM_NAME_LENGTH }
 end

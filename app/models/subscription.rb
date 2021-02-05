a42c82b79dc6f29cdcc9c4897331cfff2040516c
iogr <antgorbunov@gmail.com>
2021-02-05 22:56:25 +0300

    self_subscription, search_user_by_email validation on subscription.rb

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -15,16 +15,36 @@ class Subscription < ActiveRecord::Base
   # Или один email может использоваться только один раз (если анонимная подписка)
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  validate :search_user_by_email, unless: -> { user.present? }
+  validate :self_subscription, if: -> { user.present? }
 
   scope :persisted, -> { where('id IS NOT NULL') }
 
   # Если есть юзер, выдаем его email,
   # если нет – дергаем исходный метод
+  def user_name
+    if user.present?
+      user.name
+    else
+      super
+    end
+  end
+
   def user_email
     if user.present?
       user.email
     else
       super
+    end
+  end
+
+  def self_subscription
+    errors.add(:base, :self_subscription) if event.user == user
+  end
+
+  def search_user_by_email
+    if User.find_by(email: user_email).present?
+      errors.add(:base, :email_used, email: user_email)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :events do
     resources :comments, only: [:create, :destroy]
     resources :subscriptions, only: [:create, :destroy]
+
+    post :show, on: :member
   end
 end

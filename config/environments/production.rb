0ccec8a1ca60d599bae82a8cd0318091182a4b27
iogr <antgorbunov@gmail.com>
2021-02-14 04:14:42 +0300

    mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,8 +126,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => Rails.application.credentials.production[:user_name],
+    :password       => Rails.application.credentials.production[:password],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
